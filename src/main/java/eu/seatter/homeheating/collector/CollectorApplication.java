ca5734a61bdc7d24ec928caf5fea40b6243b5b76
james seatter <james.seatter@hotmail.co.uk>
2018-12-07 13:16:39 +0100

    Implemented Create initial Maven Java project using Spring Initializer. Closed #1

---
+++ src/main/java/eu/seatter/homeheating/collector/CollectorApplication.java
@@ -0,0 +1,12 @@
+package eu.seatter.homeheating.collector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectorApplication.class, args);
+	}
+}
