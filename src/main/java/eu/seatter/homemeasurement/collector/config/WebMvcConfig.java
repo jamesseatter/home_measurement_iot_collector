20d11cde6a384ba5ed1252e66a74857f98f2e34a
james seatter <james.seatter@hotmail.co.uk>
2020-02-11 13:22:31 +0100

    Add measurement chart to dashboard index.html #55 Add alert if unable to connect to RabbitMQ #60

---
+++ src/main/java/eu/seatter/homemeasurement/collector/config/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package eu.seatter.homemeasurement.collector.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jas
+ * Date: 09/02/2020
+ * Time: 15:19
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/"
+    };
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+}
---
+++ src/main/java/eu/seatter/homemeasurement/collector/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+//package eu.seatter.homemeasurement.collector.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * Created by IntelliJ IDEA.
+// * User: jas
+// * Date: 09/02/2020
+// * Time: 15:21
+// */
+//@Configuration
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        //Web resources
+//        web.ignoring().antMatchers("/css/**");
+//        web.ignoring().antMatchers("/scripts/**");
+//        web.ignoring().antMatchers("/images/**");
+//    }
+//}
