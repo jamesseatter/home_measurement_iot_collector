b740cc88e10d97076bd1cc1cf7cf3e0f9e599b5e
james seatter <james.seatter@hotmail.co.uk>
2020-04-17 00:52:04 +0200

    Add an Alert Service to handle General Alerts and System Alerts closes #99 NullPointerException if cache files do not already exist on Ubuntu bug closes #98

--- src/main/java/eu/seatter/homemeasurement/collector/model/SystemAlert.java
+++ src/main/java/eu/seatter/homemeasurement/collector/model/SystemAlert.java
@@ -1,8 +1,12 @@
 package eu.seatter.homemeasurement.collector.model;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.ZonedDateTimeSerializer;
 import lombok.*;
+import org.jetbrains.annotations.NotNull;
 
 import java.time.ZonedDateTime;
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,7 +19,29 @@ import java.time.ZonedDateTime;
 @Builder(toBuilder=true)
 @AllArgsConstructor
 @NoArgsConstructor
-public class SystemAlert {
-    private ZonedDateTime time;
-    private String alertMessage;
+public class SystemAlert implements Comparable<SystemAlert> {
+    private UUID alertUID;
+    private String title;
+    //    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = ZonedDateTimeSerializer.class)
+    private ZonedDateTime alertTimeUTC; // equals the measurement measurementTimeUTC
+    private String message;
+
+    public String loggerFormat() {
+        return "[" + title + "]";
+    }
+
+    @Override
+    public String toString() {
+        return "systemAlert{" +
+                "title='" + title + '\'' +
+                ", message=" + message +
+                ", measureTimeUTC=" + alertTimeUTC +
+                '}';
+    }
+
+    @Override
+    public int compareTo(@NotNull SystemAlert that) {
+        return this.alertTimeUTC.compareTo(that.alertTimeUTC);
+    }
 }
