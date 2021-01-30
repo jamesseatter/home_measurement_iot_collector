e89c0a63c649c169fd5cbf5d4a59d88b7818a732
james seatter <james.seatter@hotmail.co.uk>
2021-01-30 16:36:40 +0100

    Refactor sensorList code to use profiles to load test data via bean properties. closes #122

--- src/main/java/eu/seatter/homemeasurement/collector/services/sensor/SensorListService.java
+++ src/main/java/eu/seatter/homemeasurement/collector/services/sensor/SensorListService.java
@@ -1,69 +1,15 @@
 package eu.seatter.homemeasurement.collector.services.sensor;
 
 import eu.seatter.homemeasurement.collector.model.Measurement;
-import eu.seatter.homemeasurement.collector.sensor.listmanagers.SensorList;
-import eu.seatter.homemeasurement.collector.sensor.listmanagers.SensorListManagerJSON;
-import eu.seatter.homemeasurement.collector.sensor.listmanagers.SensorListManagerPi4J;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
  * User: jas
- * Date: 05/02/2019
- * Time: 11:35
+ * Date: 30/01/2021
+ * Time: 14:52
  */
-@Slf4j
-@Service
-public class SensorListService {
-    private final SensorList jsonFile;
-    private final SensorList pi4j;
-
-    public SensorListService(SensorListManagerJSON jsonFile, SensorListManagerPi4J pi4j) {
-        this.jsonFile = jsonFile;
-        this.pi4j = pi4j;
-    }
-
-    public List<Measurement> getSensors() {
-        log.info("Getting sensor list");
-        List<Measurement> jsonSensors = new ArrayList<>();
-        List<Measurement> finalSensorList = new ArrayList<>();
-        List<Measurement> pi4jSensors;
-
-        //The json file defines a list of non-discoverable sensors
-        //and configuration information for discoverable sensors.
-        try {
-            jsonSensors.addAll(jsonFile.getSensors());
-        } catch (Exception ex) {
-            log.error(ex.getLocalizedMessage());
-        }
-
-        try {
-            pi4jSensors = pi4j.getSensors();
-            //for each 1-wire sensor found validate that it is in the configuration file and can be measured.
-            for (Measurement pi4jSensor : pi4jSensors) {
-                boolean sensorFound = false;
-                for (Measurement jsonSensor : jsonSensors) {
-                    if (jsonSensor.getSensorid().trim().equals(pi4jSensor.getSensorid().trim())) {
-                        sensorFound = true;
-                        finalSensorList.add(jsonSensor);
-                        log.info("Sensor " + pi4jSensor.getSensorid().trim() + " found in main configuration");
-                        break;
-                    }
-                }
-                if (!sensorFound) {
-                    log.warn("Sensor " + pi4jSensor.getSensorid().trim() + " NOT found in main configuration and will not be measured. To measure the sensor value add it to the main configuration.");
-                }
-            }
-        } catch (Exception ex) {
-            log.error(ex.getLocalizedMessage());
-        }
-
-        log.info("Completed sensor list");
-
-        return finalSensorList;
-    }
+public interface SensorListService {
+    List<Measurement> getSensors();
 }
---
+++ src/main/java/eu/seatter/homemeasurement/collector/services/sensor/SensorListServiceDev.java
@@ -0,0 +1,72 @@
+package eu.seatter.homemeasurement.collector.services.sensor;
+
+import eu.seatter.homemeasurement.collector.model.Measurement;
+import eu.seatter.homemeasurement.collector.model.MeasurementUnit;
+import eu.seatter.homemeasurement.collector.model.SensorType;
+import eu.seatter.homemeasurement.collector.utils.UtilDateTime;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jas
+ * Date: 05/02/2019
+ * Time: 11:35
+ */
+@Slf4j
+@Service
+@Profile("dev")
+public class SensorListServiceDev implements SensorListService {
+
+    @Override
+    public List<Measurement> getSensors() {
+        log.warn("Test sensor list in use");
+        List<Measurement> list = new ArrayList<>();
+        list.add(Measurement.builder()
+                .recordUID(UUID.randomUUID())
+                .sensorid("28-000000000001")
+                .title("Température de l'eau à l'arrivée")
+                .description("Returns the temperature of the hot water entering the house from the central heating system")
+                .familyid(40)
+                .sensorType(SensorType.ONEWIRE)
+                .low_threshold(45.0)
+                .high_threshold(60.0)
+                .alertgroup("temperature_threshold_alerts_private")
+                .alertdestination("BORRY")
+                .build());
+
+        list.add(Measurement.builder()
+                .recordUID(UUID.randomUUID())
+                .sensorid("28-000000000002")
+                .title("Température de l'eau de chaudière")
+                .description("Returns the temperature of the hot water in the boiler")
+                .familyid(40)
+                .sensorType(SensorType.ONEWIRE)
+                .low_threshold(35.0)
+                .high_threshold(60.0)
+                .alertgroup("temperature_threshold_alerts_private")
+                .alertdestination("PRIVATE")
+                .build());
+
+        return list;
+    }
+
+    public static List<Measurement> testData(List<Measurement> sensorList) {
+        log.warn("Test measurement data in use");
+        for(Measurement srec : sensorList) {
+            srec.setMeasurementUnit(MeasurementUnit.C);
+            srec.setMeasureTimeUTC(UtilDateTime.getTimeDateNowNoSecondsInUTC());
+            int val = ThreadLocalRandom.current().nextInt(35, 75);
+            srec.setValue((double)val);
+        }
+
+        return Collections.unmodifiableList(sensorList);
+    }
+}
