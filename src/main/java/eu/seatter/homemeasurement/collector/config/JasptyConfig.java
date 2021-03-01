543965a2971066d5c8d99e5bc5f3a80cd6c5d72c
james seatter <james.seatter@hotmail.co.uk>
2021-03-01 22:52:21 +0100

    Implement Jasypt and encrypt passwords in the application properties files closes #138

---
+++ src/main/java/eu/seatter/homemeasurement/collector/config/JasptyConfig.java
@@ -0,0 +1,15 @@
+package eu.seatter.homemeasurement.collector.config;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jas
+ * Date: 01/03/2021
+ * Time: 22:46
+ */
+@Configuration
+@EnableEncryptableProperties()
+public class JasptyConfig {
+}
